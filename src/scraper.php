e9488044cc637a5a16ac9d5a24ec54cd3c17ee4c
Ludmi <ludmit@outlook.com>
2024-01-15 11:48:03 -0300

    feat: :sparkles: iterating trough all the shop

--- src/scraper.php
+++ src/scraper.php
@@ -32,32 +32,53 @@ foreach ($paginationElements as $paginationElement) {
         $paginationLinks[] = $paginationLink;
     }
 }
-// Print the paginationLinks array
-print_r($paginationLinks);
 
 // remove all non-numeric characters in the last element of the paginationLinks
 // array to retrieve the highest pagination number
 $highestPaginationNumber = preg_replace("/\D/", "", end($paginationLinks)); 
-print_r($highestPaginationNumber);
 
-$productDataLit = array();
+$productDataList = array();
 // retrieve the list of products on the page
 $productElements = $htmlDomParser->find("li.product");
-foreach ($productElements as $productElement) {
-    // extract the product data
-    $url = $productElement->findOne("a")->getAttribute("href");
-    $image = $productElement->findOne("img")->getAttribute("src");
-    $name = $productElement->findOne("h2")->text;
-    $price = $productElement->findOne(".price span")->text;
+// Initialize the product data array
+// Initialize the product data array
+$productDataList = [];
 
-    $productData = array(
-        "url" => $url,
-        "image" => $image,
-        "name"=> $name,
-        "price" => $price
-    );
-    
-    $productDataList[] = $productData;  
-    print_r($productDataList);
+// Iterate over all "/shop/page/X" and return all product data
+for ($paginationNumber = 1; $paginationNumber <= $highestPaginationNumber; $paginationNumber++) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://scrapeme.live/shop/page/{$paginationNumber}/");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $pageHtml = curl_exec($curl);
+    curl_close($curl);
+
+    // Check if the request was successful
+    if ($pageHtml !== false) {
+        // Parse the HTML
+        $paginationHtmlDomParser = HtmlDomParser::str_get_html($pageHtml);
+
+        // Find product elements on the page
+        $productElements = $paginationHtmlDomParser->find('.product');
+
+        // Extract data from each product element and add it to the list
+        foreach ($productElements as $productElement) {
+            // Extract the product data and add it to the list
+            $productDataList[] = [
+                "url" => $productElement->findOne("a")->getAttribute("href"),
+                "image" => $productElement->findOne("img")->getAttribute("src"),
+                "name" => $productElement->findOne("h2")->text,
+                "price" => $productElement->findOne(".price span")->text
+            ];
+        }
+    }
+    // Output the product data as JSON
+echo json_encode($productDataList);
+
 }
+
+
+
+
+
+
 
